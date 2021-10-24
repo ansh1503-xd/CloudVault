7244a44dfb2b8238337e31722e4bd0ce9655f18b
ansh1503-xd <anshmayani1503@gmail.com>
2021-10-24 23:54:12 +0530

    Added more tab and UI

--- CloudVault/app/src/main/java/com/sminfotech/cloudvault/MainActivity.java
+++ CloudVault/app/src/main/java/com/sminfotech/cloudvault/MainActivity.java
@@ -3,17 +3,26 @@ package com.sminfotech.cloudvault;
 import static com.sminfotech.cloudvault.SplashActivity.sp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.hardware.biometrics.BiometricManager;
+import android.hardware.biometrics.BiometricPrompt;
+import android.hardware.biometrics.BiometricPrompt.AuthenticationCallback;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,18 +31,19 @@ public class MainActivity extends AppCompatActivity {
     FrameLayout bottomTabContainer;
     Fragment fragment = null;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         loginuid = sp.getString("uid", "");
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomTabContainer = findViewById(R.id.bottomTabContainer);
         bottomNavigationView.setSelectedItemId(R.id.tab_home);
         fragment = new HomeFragment();
         replaceFragment(fragment);
+
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -51,17 +61,11 @@ public class MainActivity extends AppCompatActivity {
                         fragment = new EarnFragment();
                         replaceFragment(fragment);
                         break;
+                    case R.id.tab_more:
+                        fragment = new MoreFragment();
+                        replaceFragment(fragment);
+                        break;
                 }
-//                if (item.equals(R.id.tab_home)) {
-//                    Fragment fragment = new HomeFragment();
-//                    replaceFragment(fragment);
-//                }else if (item.equals(R.id.tab_profile)){
-//                    Fragment fragment = new ProfileFragment();
-//                    replaceFragment(fragment);
-//                }else if (item.equals(R.id.tab_wallet)){
-//                    Fragment fragment = new EarnFragment();
-//                    replaceFragment(fragment);
-//                }
                 return true;
             }
         });
